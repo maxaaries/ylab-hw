9f2445fcbaf363e967ef2154b6bce2f35ae0d8e8
maxaaries <108334673+maxaaries@users.noreply.github.com>
2022-06-28 19:40:08 +0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,9 +1,7 @@
 
-from ipaddress import IPv4Address
-
-
 def int32_to_ip(int32):
-    return IPv4Address(int32)
+    bin_str = f'{int32:b}'.rjust(32, '0')
+    return '.'.join([str(int(bin_str[idx:idx + 8], 2)) for idx in range(0, len(bin_str), 8)])
 
 
 assert int32_to_ip(2154959208) == "128.114.17.104"
